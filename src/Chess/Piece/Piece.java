ae42e8d3566cf04dcc13f290b7e04bfca13e4eee
fuzzy <60215646+CathalOleary@users.noreply.github.com>
2022-11-12 21:14:56 +0000

    update5

--- src/Chess/Piece/Piece.java
+++ src/Chess/Piece/Piece.java
@@ -1,12 +1,14 @@
 package Chess.Piece;
 
+import Chess.Board.*;
+
 import java.util.List;
 
 /**
  * Created by Cathal OLeary
  */
 
-public class Piece {
+public abstract class Piece {
     private final int PiecePosition;
     private final Alliance PieceAlliance;
 
@@ -15,5 +17,5 @@ public class Piece {
         this.PieceAlliance=PieceAlliance;
     }
 
-   public abstract List<Moves> CalLegaMoves(final Board board);
+   public abstract List<Move> CalLegaMoves(final Board board);
 }
